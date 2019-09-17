4d44b26d3fef8ab47e1d0c5a0b2ad77da017d605
Harry Hartmann <hry@solaris-it.com>
2019-09-17 10:55:42 -0400

    change message

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	log.Println("Starting pipeline application...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world!\n")
+		fmt.Fprintf(w, "Hello hryatgit!\n")
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
